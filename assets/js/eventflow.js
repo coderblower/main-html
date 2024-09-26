6142b1e44a9d25b26fe0a5ba7ab7fc54903469e8
saiful110816 <saiful110816@acc.edu.bd>
2024-09-26 20:26:51 +0600

    sfsdfsds

--- assets/js/eventflow.js
+++ assets/js/eventflow.js
@@ -1052,3 +1052,20 @@ canvas.addEventListener( 'mouseup', function( e ) {
 // once the window loads, we are ready for some fireworks!
 window.onload = loop;
 
+
+
+// The End Of Year Date To Countdown Date
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Unix timestamp (in seconds) to count down to
+  var toDayFromNow = (new Date("Dec 31, 2024 23:59:59").getTime() / 1000) + (3600 / 60 / 60 / 24) - 1;
+  // Set Up FlipDown
+  var flipdown = new FlipDown(toDayFromNow)
+
+  // Start The Count Down
+  .start()
+  // Do Something When The Countdown Ends
+  .ifEnded(() => {
+      document.querySelector(".flipdown").innerHTML = `<h2>Timer is ended</h2>`;
+  });
+});
